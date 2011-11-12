23a8b30e71c5e59e0b6d906f3a6e165b2f11de5c
Edvinas Bartkus <edvinas@geeks.lt>
2011-11-12 15:52:59 +0200

    Category and Cate

---
+++ src/lt/appcamp/appcamp16/services/PhotoAdapter.java
@@ -0,0 +1,27 @@
+package lt.appcamp.appcamp16.services;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class PhotoAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 50;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return new Long(i);
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
