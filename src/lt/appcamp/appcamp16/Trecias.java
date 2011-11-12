d35b414e1e750d2011354fad5496685df51a4352
Laurynas Butkus <laurynas.butkus@gmail.com>
2011-11-12 14:55:44 +0200

    Gallery

--- src/lt/appcamp/appcamp16/Trecias.java
+++ src/lt/appcamp/appcamp16/Trecias.java
@@ -3,12 +3,16 @@ package lt.appcamp.appcamp16;
 import lt.appcamp.appcamp16.ui.CoverFlow;
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.Gallery;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Trecias extends Activity
 {
@@ -16,16 +20,13 @@ public class Trecias extends Activity
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.trecias);
+        setContentView(R.layout.trecias);
 
-        CoverFlow coverFlow;
-        coverFlow = new CoverFlow(this);
+        CoverFlow coverFlow = (CoverFlow) findViewById(R.id.gallery);
         
         coverFlow.setAdapter(new ImageAdapter(this));
 
         ImageAdapter coverImageAdapter =  new ImageAdapter(this);
-        
-        //coverImageAdapter.createReflectedImages();
         
         coverFlow.setAdapter(coverImageAdapter);
         
@@ -33,17 +34,16 @@ public class Trecias extends Activity
         coverFlow.setSelection(4, true);
         coverFlow.setAnimationDuration(1000);
         
-        
-        setContentView(coverFlow);
-        
-//        Gallery gallery = (Gallery) findViewById(R.id.gallery);
-//        gallery.setAdapter(new ImageAdapter(this));
+        coverFlow.setOnItemClickListener(new OnItemClickListener() {
 
-//        gallery.setOnItemClickListener(new OnItemClickListener() {
-//            public void onItemClick(AdapterView parent, View v, int position, long id) {
-//                Toast.makeText(Trecias.this, "" + position, Toast.LENGTH_SHORT).show();
-//            }
-//        });
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position,
+                    long id) {
+                
+                Toast.makeText(Trecias.this, "" + position, Toast.LENGTH_SHORT).show();
+                
+            }
+        });
     }
     
     public class ImageAdapter extends BaseAdapter {
@@ -61,10 +61,10 @@ public class Trecias extends Activity
 
         public ImageAdapter(Context c) {
             mContext = c;
-//            TypedArray attr = mContext.obtainStyledAttributes(R.styleable.Trecias);
-//            mGalleryItemBackground = attr.getResourceId(
-//                    R.styleable.HelloGallery_android_galleryItemBackground, 0);
-//            attr.recycle();
+            TypedArray attr = mContext.obtainStyledAttributes(R.styleable.ProductsGallery);
+            mGalleryItemBackground = attr.getResourceId(
+                    R.styleable.ProductsGallery_android_galleryItemBackground, 0);
+            attr.recycle();
         }
 
         public int getCount() {
